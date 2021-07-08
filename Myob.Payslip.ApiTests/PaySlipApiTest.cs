071a788d1f5441e76be9c6c3aaf3d1f8ac8e8be8
Meng Zhang <meng.zhang@myob.com>
2021-07-08 22:40:49 +1000

    Added unit test for WebApi

---
+++ Myob.Payslip.ApiTests/PaySlipApiTest.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Myob.Payslip.Api.Controllers;
+using Myob.Payslip.Api.Models;
+using Xunit;
+
+namespace Myob.Payslip.ApiTests
+{
+    public class PaySlipApiTest
+    {
+        private PayDetailsRequest testResquest = new PayDetailsRequest();
+        private readonly ILogger<PayDetailsContoller> _logger;
+
+        [Fact]
+        public async Task shouldReturnStatus200WhenRequestIsValid()
+        {
+            // Arrange
+            testResquest.FirstName = "Meng";
+            testResquest.SurName = "Zhang";
+            testResquest.AnnualSalary = "60000";
+            testResquest.SuperRate = "9.5";
+            testResquest.PayStartDate = "sss";
+            testResquest.PayEndDate = "eee";
+            var mockController = new PayDetailsContoller(_logger);
+            // Act
+            var actionResult = mockController.Post(testResquest);
+            var okResult = actionResult as OkObjectResult;
+            Assert.Equal(200, okResult.StatusCode);
+            Assert.Equal(
+                JsonSerializer.Serialize(new PayDetailsRequest.PayDetailsResponse(
+                    "sss - eee",
+                    "Meng Zhang",
+                    5000,
+                    921,
+                    4079,
+                    475,
+                    200
+                )), okResult.Value);
+        }
+
+        [Fact]
+        public async Task shouldReturnStatus422WhenRequestIsValid()
+        {
+            // Arrange
+            testResquest.FirstName = "Meng";
+            testResquest.SurName = "Zhang";
+            testResquest.AnnualSalary = "60000oo";
+            testResquest.SuperRate = "9.5";
+            testResquest.PayStartDate = "sss";
+            testResquest.PayEndDate = "eee";
+            var mockController = new PayDetailsContoller(_logger);
+            // Act
+            var actionResult = mockController.Post(testResquest);
+            var UnprocessableEntityResult = actionResult as UnprocessableEntityResult;
+            Assert.Equal(422, UnprocessableEntityResult.StatusCode);
+
+        }
+    }
+}
